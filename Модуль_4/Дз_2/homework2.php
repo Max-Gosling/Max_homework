983682b20886bec95f54d16b941122b624c3dd7a
Max-Gosling <maxim.nevinsky@yandex.ru>
2023-09-20 18:39:38 +0300

    serhdfvdshtfxvfdgcbftjfvbthdrthtghtrh

---
+++ Модуль_4/Дз_2/homework2.php
@@ -0,0 +1,9 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+
+if($_SERVER['REQUEST_METHOD'] == 'POST' && $_SERVER["CONTENT_TYPE"] == 'application/json') {
+	$postData = file_get_contents('php://input');
+	$data = json_decode($postData, true);
+	echo json_encode($data);
+}
+?>
